25044ceeead801217c3de849dbe75773fe19ff6e
Codemikel <mdrssog@protonmail.com>
2021-07-30 20:17:04 -0500

    add variables to title

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="{{asset('Bifix-shop-logo.svg')}}" type="image/x-icon">
 
-        <title>Laravel</title>
+        <title>{{$title}} | Bifix Shop</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('index', ['title' => 'Home']);
 });
 
 Route::get('/login', function (){
